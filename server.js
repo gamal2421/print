7074614ec2e4fc356f5d35db14f3b804eb68b24d
gamal2421 <gamal29211@gmail.com>
2026-09-24 13:09:33 +0300

    ok

--- server.js
+++ server.js
@@ -274,7 +274,7 @@ public sealed class ImagePrinter : IDisposable {
             document.PrinterSettings.PrinterName = printerName;
         }
 
-        document.DefaultPageSettings.Landscape = true;
+        document.DefaultPageSettings.Landscape = false;
         document.DefaultPageSettings.Margins = new Margins(0, 0, 0, 0);
 
         document.PrintPage += PrintPage;
